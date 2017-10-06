4ae6c1580af3b24b5f38f28e1a8b86aeb82dddc9
Andre <apcianflone@gmail.com>
2017-10-06 08:02:34 -0400

    refac

--- prepare.py
+++ prepare.py
@@ -1,47 +1,68 @@
 # author: andre cianflone
 # usage:
-#$ python prepare.py archive.zip
-import os, zipfile, shutil
-fold = zipfile.ZipFile('archive.zip')
+# python prepare.py archive.zip a1
 
-# Extract
-fold.extractall()
+import os, zipfile, shutil, sys
+archive = sys.argv[1]
+dest = sys.argv[2]
 
-# Extract assignments and delete archive
-print('extracting assignments and moving to student folders')
-for f in fold.namelist():
-  student_dir = '/'.join(f.split("/")[:2])
-  if f.endswith('.zip'):
-    assignment = zipfile.ZipFile(f)
-    assignment.extractall(student_dir)
-    os.remove(f)
-  if f.endswith('.7z'):
-    os.system( '7z x ' + f + ' -o' + student_dir)
-    os.remove(f)
+# Setup root dir
+if os.path.exists(dest):
+  shutil.rmtree(dest)
+os.makedirs(dest)
 
-# Move up to student dir
-for f in fold.namelist():
-  if 'c_henneb' in f:
-    a=1
-  student_dir = '/'.join(f.split("/")[:2])
-  source = '/'.join(f.split("/")[:-1])
-  files = os.listdir(source)
-  for fi in files:
-    path = source + "/" + fi
-    if os.path.exists(path):
-      shutil.move(os.path.join(source, fi), os.path.join(student_dir, fi))
+# Keep list of (id, user_name)
+users = []
 
-# Remove useless dirs
-print('cleaning up directories')
-for f in fold.namelist():
-  useless = '/'.join(f.split("/")[:3])
-  if os.path.exists(useless):
-    shutil.rmtree(useless)
+# Extract original archive
+print('extracting assignments')
+with zipfile.ZipFile(archive) as zip_f:
+  # Get unique user dirs
+  temp = []
+  for path in zip_f.namelist():
+    temp.append(path.split("/")[1])
+  temp = set(temp)
+  for user in temp:
+    # build list of (id, user_name)
+    users.append(user.split("-"))
+
+  # Go through items in zip
+  for item in zip_f.namelist():
+    filename = os.path.basename(item)
+    # skip directories
+    if not filename:
+      continue
+
+    # Prepare student dir
+    student_dir = item.split("/")[1].split("-")[1]
+    student_dir = os.path.join(dest, student_dir)
+    if not os.path.exists(student_dir):
+      os.makedirs(student_dir)
+
+    # Copy file
+    source = zip_f.open(item)
+    with open(os.path.join(student_dir, filename), "wb") as target:
+      with source, target:
+        shutil.copyfileobj(source, target)
+
+# Extract individual student archives
+for st_dir in os.listdir(dest):
+  st_dir = os.path.join(dest,st_dir)
+  for fi in os.listdir(st_dir):
+    fi_path = os.path.join(st_dir, fi)
+    if fi.endswith('.zip'):
+      assignment = zipfile.ZipFile(fi_path)
+      assignment.extractall(st_dir)
+      os.remove(fi_path)
+    if fi.endswith('.7z'):
+      os.system( '7z x ' + fi_path + ' -o' + st_dir)
+      os.remove(fi_path)
 
 # Student ID file
-id_file = fold.namelist()[0].split("/")[0] + '/' + 'students.txt'
+id_file = os.path.join(dest, 'students.csv')
 with open(id_file, 'w') as f:
-  for student in [x.split("/")[1] for x in fold.namelist()]:
-    f.write(student+'\n')
-
+  for st_id, user_name in users:
+    f.write(user_name + ',' + st_id + '\n')
 print('created student id file: ', id_file)
+
+
